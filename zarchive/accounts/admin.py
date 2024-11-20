d5a9d3347b86036f7c307c5471aa2d488644e756
kibaa9 <95776495+kibaa9@users.noreply.github.com>
2024-11-20 23:16:22 +0200

    Added admin panel

--- zarchive/accounts/admin.py
+++ zarchive/accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from zarchive.accounts.forms import AppUserChangeForm, AppUserForm
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class AppUserAdmin(UserAdmin):
+    add_form = AppUserForm
+    form = AppUserChangeForm
+
+    list_display = ('username', 'email', 'is_active')
+
+    fieldsets = (
+        ('Credentials', {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (None, {'fields': ('is_active', )})
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide', ),
+                'fields': ('username', 'email', 'password1', 'password2'),
+            },
+        ),
+    )
